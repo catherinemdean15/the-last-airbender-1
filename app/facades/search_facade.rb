2ca4ceec749de4579a5c17f1905aa46395213906
Catherine Dean <catherinemdean15@gmail.com>
2021-02-12 10:56:00 -0700

    include second page

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -1,7 +1,14 @@
 class SearchFacade
   class << self
-    def search_for_members(nation)
-      data = SearchService.call_for_members(nation)
+    def search_for_fire_nation
+      # ideally this would be much more dynamic in case number of members were to change
+      page = 1
+      data = []
+      until page == 6
+        data << SearchService.call_for_members(page)
+        page += 1
+      end
+      data = data.flatten
       Search.new(data)
     end
   end
