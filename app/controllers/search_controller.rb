a76ee34d2cf3803e8004e4581b3837c6cb458e8a
Catherine Dean <catherinemdean15@gmail.com>
2021-02-12 10:55:45 -0700

    remove typo

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
 class SearchController < ApplicationController
   def index
-    @members = SearchFacade.search_for_fire_nation if params[:nation] == fire_nation
+    @nation = params[:nation]
+    @members = SearchFacade.search_for_fire_nation if params[:nation] == 'fire_nation'
     # named search_for_fire_nation for futue functionality of searching for other nations
   end
 end
